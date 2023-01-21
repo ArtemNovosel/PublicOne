68cd49e955d8ed41f230784e83eeff5b61491a33
ArtemNovosel <arteimn@mail.ru>
2023-01-21 17:54:01 +0500

    'tests'

---
+++ test.py
@@ -0,0 +1,25 @@
+# a = int(input())
+# b = 1
+# while b < 11:
+#     print(b, "*", a, "=", b * a)
+#     b = b + 1
+# # def multi_table(a):
+# #     return # good luck
+
+# data = input()
+# s = []
+# for i in data:
+#     if data.count(i) != 1: #проверяем значение в списке
+#         s.append(i) #добавляем в список i
+# print(s)
+
+# data = input()
+# s = []
+# for i in data:
+#     if data.count(i) != 1:  # проверяем значение в списке
+#         s.append(i)  # добавляем в список i
+# if s == []:
+#    print("изограмма")
+# else:
+#     print("не изограмма")
+
